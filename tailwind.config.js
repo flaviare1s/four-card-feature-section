b6bb784f311b827d574aff3224d89e4acf47a327
Flávia Reis <flaviarster@gmail.com>
2024-02-05 04:44:40 -0300

    Add colors and fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: 'hsl(0, 78%, 62%)',
+        cyan: 'hsl(180, 62%, 55%)',
+        orange: 'hsl(34, 97%, 64%)',
+        blue: 'hsl(212, 86%, 64%)',
+        veryDarkBlue: 'hsl(234, 12%, 34%)',
+        grayishBlue: 'hsl(229, 6%, 66%)',
+        veryLightGray: 'hsl(0, 0%, 98%)'
+      },
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
